37f89be23d270ce747b6affa1a6e34159688acef
Kevin Peters <git.igeligel@googlemail.com>
2017-06-02 22:32:18 +0200

    feat(project)
    initial commit

---
+++ covfefe/CovfefeUtilities.cs
@@ -0,0 +1,15 @@
+﻿namespace covfefe
+{
+    public static class CovfefeUtilities
+    {
+        public static string Covfefe(string input)
+        {
+            return $"{input} covfefe";
+        }
+
+        public static string Translate(this string input)
+        {
+            return input.Replace("coverage", "covfefe");
+        }
+    }
+}
